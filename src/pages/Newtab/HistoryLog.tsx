00a6dd7d87bad2ed3de460f79f1ce7fc0d4671aa
LeoVS09 <leovs010@gmail.com>
2020-12-30 02:17:46 +0300

    pretify hisory log page

---
+++ src/pages/Newtab/HistoryLog.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Newtab.scss';
+import { PageDataDictanory } from '../../types'
+import { PageData, PageVisit } from '../../history';
+
+export interface HistoryLogProps {
+    pages: PageDataDictanory
+    history: Array<PageVisit>
+}
+
+export const HistoryLog: React.FC<HistoryLogProps> = ({ pages, history }) => (
+    <div className="history-log">
+        <h1>Mind history log</h1>
+        <p>Fount items {history.length}</p>
+        <ul>
+            {history.map((visit, i) => {
+                const fromPage = visit.from ? pages[visit.from] : undefined;
+                const toPage = visit.to ? pages[visit.to] : undefined;
+
+                return <li key={`${visit.from}:${visit.to}:${i}`}>From: <PageLink url={visit.from} page={fromPage} /> {'->'} to <PageLink url={visit.to} page={toPage} /></li>
+            })}
+        </ul>
+    </div>
+);
+
+const PageLink: React.FC<{ url?: string, page?: PageData }> = ({ url, page }) => {
+    if (url && page?.title) {
+        return <a href={url}>{page.title}</a>
+    }
+
+    if (url) {
+        return <a href={url}>{url}</a>
+    }
+
+    if (page?.title) {
+        return <a href='#'>{page.title}</a>
+    }
+
+    return <a href='#'>Unknown</a>
+}
--- src/pages/Newtab/Newtab.tsx
+++ src/pages/Newtab/Newtab.tsx
@@ -1,49 +1,37 @@
-import React from 'react';
-// @ts-ignore
-import logo from '../../assets/img/logo.svg';
-import './Newtab.css';
+import React, { useState } from 'react';
 import './Newtab.scss';
 import { PageDataDictanory } from '../../types'
 import { PageData, PageVisit } from '../../history';
+import { HistoryLog } from './HistoryLog';
+
+export enum Pages {
+  HISTORY_LOG = 'History Log'
+}
 
 export interface NewTabProps {
   pages: PageDataDictanory
   history: Array<PageVisit>
 }
 
-const Newtab: React.FC<NewTabProps> = ({ pages, history }) => {
+const Newtab: React.FC<NewTabProps> = ({ pages, history }) => (
+  <div className="App">
+    <Router
+      pages={pages}
+      history={history}
+    />
+  </div>
+);
+
+const Router: React.FC<NewTabProps> = (props) => {
+  const [page, toPage] = useState(Pages.HISTORY_LOG);
+
+  if (page === Pages.HISTORY_LOG) {
+    return <HistoryLog {...props} />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Mind history log</h1>
-        <p>Fount items {history.length}</p>
-        <ul>
-          {history.map(visit => {
-            const fromPage = visit.from ? pages[visit.from] : undefined;
-            const toPage = visit.to ? pages[visit.to] : undefined;
-
-            return <li>From: <PageLink url={visit.from} page={fromPage} /> {'->'} to <PageLink url={visit.to} page={toPage} /></li>
-          })}
-        </ul>
-      </header>
-    </div>
-  );
-};
-
-const PageLink: React.FC<{ url?: string, page?: PageData }> = ({ url, page }) => {
-  if (url && page?.title) {
-    return <a href={url}>{page.title}</a>
-  }
-
-  if (url) {
-    return <a href={url}>{url}</a>
-  }
-
-  if (page?.title) {
-    return <a href='#'>{page.title}</a>
-  }
-
-  return <a href='#'>Unknown</a>
+    <h1>Not found</h1>
+  )
 }
 
 export default Newtab;
