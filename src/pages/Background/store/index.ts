7126bc5142138e8fa099b402c95320877c55070c
LeoVS09 <leovs010@gmail.com>
2021-01-05 00:16:23 +0300

    save state in local storage

--- src/pages/Background/store/index.ts
+++ src/pages/Background/store/index.ts
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { pagesReducer } from './reducer'
+import * as persistence from './persistence'
 
 export * as actions from './actions'
 
 export const store = configureStore({
-    reducer: pagesReducer
+    reducer: pagesReducer,
+    preloadedState: persistence.retrive()
+})
+
+store.subscribe(() => {
+    persistence.save(store.getState())
 })
---
+++ src/pages/Background/store/persistence.ts
@@ -0,0 +1,23 @@
+
+const LOCAL_STORAGE_KEY = 'MIND_HISTORY'
+
+export function save(state: Object) {
+    try {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state))
+    } catch (err) {
+        console.error('Cannot save state, history will be lost when browser will be closed\n', err)
+    }
+}
+
+export function retrive<T extends Object>(): T | undefined {
+    try {
+        const item = localStorage.getItem(LOCAL_STORAGE_KEY)
+        if (!item) {
+            return
+        }
+
+        return JSON.parse(item)
+    } catch (err) {
+        console.error('Cannot recover state from local storage\n', err)
+    }
+}
