0109760bd62dd8556029ff173dd67317eaec12c4
leovs09 <leovs010@gmail.com>
2021-02-01 19:31:37 +0000

    fix(graph-page): resolve warning from cytoscape when not have favicon url

--- src/pages/Graph/views/MindGraph.tsx
+++ src/pages/Graph/views/MindGraph.tsx
@@ -147,37 +147,45 @@ const countEdges = (node: NodeDefinition, edges: Array<EdgeDefinition>): number 
 //     return Math.floor(Math.random() * (max - min + 1)) + min
 // }
 
-const graphStyles = [{
-    "selector": "core",
-    "style": {
-        "selection-box-color": "#AAD8FF",
-        "selection-box-border-color": "#8BB0D0",
-        "selection-box-opacity": "0.5"
-    }
-}, {
-    "selector": "node",
-    "style": {
-        "width": "mapData(score, 1, 5, 30, 60)",
-        "height": "mapData(score, 1, 5, 30, 60)",
-        "label": "data(label)",
-        "font-size": "14px",
-        "text-valign": "bottom",
-        "text-halign": "center",
-        "background-color": "#777",
-        "background-image": 'data(favIconUrl)',
-        "color": "#fff",
-        "overlay-padding": "6px",
-        "z-index": "10"
-    }
-}, {
-    "selector": "edge",
-    "style": {
-        'width': 1,
-        "curve-style": "haystack",
-        "haystack-radius": "0.1",
-        "opacity": "0.4",
-        "line-color": "red",
-        "overlay-padding": "5px"
-    }
-},
+const graphStyles = [
+    {
+        "selector": "core",
+        "style": {
+            "selection-box-color": "#AAD8FF",
+            "selection-box-border-color": "#8BB0D0",
+            "selection-box-opacity": "0.5"
+        }
+    },
+    {
+        "selector": "node",
+        "style": {
+            "width": "mapData(score, 1, 5, 30, 60)",
+            "height": "mapData(score, 1, 5, 30, 60)",
+            "label": "data(label)",
+            "font-size": "14px",
+            "text-valign": "bottom",
+            "text-halign": "center",
+            "background-color": "#777",
+            "color": "#fff",
+            "overlay-padding": "6px",
+            "z-index": "10"
+        }
+    },
+    {
+        "selector": "node[favIconUrl]",
+        "style": {
+            "background-image": 'data(favIconUrl)'
+        }
+    },
+    {
+        "selector": "edge",
+        "style": {
+            'width': 1,
+            "curve-style": "haystack",
+            "haystack-radius": "0.1",
+            "opacity": "0.4",
+            "line-color": "red",
+            "overlay-padding": "5px"
+        }
+    },
 ]
